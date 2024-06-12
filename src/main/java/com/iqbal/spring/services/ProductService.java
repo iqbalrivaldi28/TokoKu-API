64fa6c9d171f153a82e2707c261a5bd1e71bd75f
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-06-12 10:56:38 +0700

    feat: add services productservice

---
+++ src/main/java/com/iqbal/spring/services/ProductService.java
@@ -0,0 +1,32 @@
+package com.iqbal.spring.services;
+
+import com.iqbal.spring.model.entity.Product;
+import com.iqbal.spring.model.repository.ProductRepo;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@Transactional
+public class ProductService {
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    public Product save(Product product){
+        return productRepo.save(product);
+    }
+
+    public Product findOne(Long id){
+        return productRepo.findById(id).get();
+    }
+
+    public Iterable<Product> findAll(){
+        return productRepo.findAll();
+    }
+
+    public void removeOne(Long id){
+        productRepo.deleteById(id);
+    }
+}
