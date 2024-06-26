28a1cef9536914c73e3bfa7ed4805d17bd694200
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-06-26 10:47:21 +0700

    feat: add categoryRepo and suplierRepo

---
+++ src/main/java/com/iqbal/spring/model/repository/SuplierRepo.java
@@ -0,0 +1,7 @@
+package com.iqbal.spring.model.repository;
+
+import com.iqbal.spring.model.entity.Suplier;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SuplierRepo extends CrudRepository<Suplier, Long> {
+}
