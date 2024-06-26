8fd935db39258b8c3c9e4588050c1d42e764a612
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-06-26 10:57:29 +0700

    feat: add methods in categoryService

--- src/main/java/com/iqbal/spring/services/CategoryService.java
+++ src/main/java/com/iqbal/spring/services/CategoryService.java
@@ -1,9 +1,12 @@
 package com.iqbal.spring.services;
 
+import com.iqbal.spring.model.entity.Category;
 import com.iqbal.spring.model.repository.CategoryRepo;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -11,6 +14,30 @@ public class CategoryService {
 
     @Autowired
     private CategoryRepo categoryRepo;
+
+    public Category save(Category category){
+        return categoryRepo.save(category);
+    }
+
+    public Category findOne(Long id){
+        Optional<Category> category =  categoryRepo.findById(id);
+
+        if (!category.isPresent()){
+            return  null;
+        }
+
+        return category.get();
+    }
+
+    public Iterable<Category> findAll() {
+        return categoryRepo.findAll();
+    }
+
+    public void removeOne(Long id){
+        categoryRepo.deleteById(id);
+    }
+
+
 
 
 }
